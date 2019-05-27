cde9fc79e8f91748beb1f5857731c28884f6500d
horseTom <guozongren1107@163.com>
2019-05-27 17:18:16 +0800

    Day02

--- Day04.py
+++ Day04.py
@@ -21,10 +21,43 @@
 # if counter > 7:
 #     print("你的智商余额明显不足")
 
-#输出乘法口诀
+# 输出乘法口诀
 
-for i in range(1,10):
-    for j in range(1,i+1):
-        print('%d * %d = %d' % (i, j, i*j), end='\t')
-    print()
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print('%d * %d = %d' % (i, j, i*j), end='\t')
+#     print()
+
+# 打印三角形
+# row = int(input("请输入行数："))
+# for i in range(row):
+#     for _ in range(row-i-1):
+#         print(' ', end='')
+#     for _ in range(2 * i + 1):
+#         print('*', end='')
+#     print()
+
+# for i in range(row):
+# #     for _ in range(row - i):
+# #         print(' ',end='')
+# #     for _ in range(i + 1):
+# #         print('*',end='')
+# #     print()
+
+# 输入一个正整数判断他是否是素数
+from math import sqrt
+num = int(input("请输入一个正整数："))
+end = int(sqrt(num))
+print(end)
+is_prime = True
+for x in range(2, end+1):
+    if num % x == 0:
+        is_prime = False
+        break
+if is_prime and num != 1:
+    print('%d是素数' % num)
+else:
+    print('%d不是素数' % num)
+
+
 
